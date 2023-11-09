3f9c5bd8327e90aa7b9dae8dff673753778fd876
carmcc <carmine.marchesani@studenti.unicam.it>
2023-11-09 17:35:23 +0100

    Updated pipeline config

--- ve_pipeline.cpp
+++ ve_pipeline.cpp
@@ -6,9 +6,12 @@
 using namespace std;
 
 namespace ve {
-    VePipeline::VePipeline(const string &vertFilePath, const string &fragFilePath)
+    VePipeline::VePipeline(VeDevice &device,
+                           const std::string &vertFilePath,
+                           const std::string &fragFilePath,
+                           const PipelineConfigInfo &configInfo) : veDevice{device}
     {
-        createGraphicsPipeline(vertFilePath,fragFilePath);
+        createGraphicsPipeline(vertFilePath,fragFilePath, configInfo);
     }
     vector<char> VePipeline::readFile(const string &filePath)
     {
@@ -26,11 +29,30 @@ namespace ve {
         file.close();
         return buffer;
     }
-    void VePipeline::createGraphicsPipeline(const string &vertFilePath, const string &fragFilePath) {
+    void VePipeline::createGraphicsPipeline(const string &vertFilePath, const string &fragFilePath, const PipelineConfigInfo &configInfo){
         auto vertCode = readFile(vertFilePath);
         auto fragCode = readFile(fragFilePath);
 
         cout << "Vertex size: " << vertCode.size() << endl;
         cout << "Fragment size: " << fragCode.size() << endl;
     }
+
+    void VePipeline::createShaderModule(const vector<char> &code, VkShaderModule *shaderModule)
+    {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t *>(code.data());
+
+        if (vkCreateShaderModule(veDevice.device(),&createInfo,nullptr,shaderModule) != VK_SUCCESS) {
+            throw runtime_error("failed to create shader module!");
+        }
+    }
+
+    PipelineConfigInfo VePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height)
+    {
+        PipelineConfigInfo configInfo{};
+        return configInfo;
+    }
+
 }
--- ve_pipeline.hpp
+++ ve_pipeline.hpp
@@ -1,15 +1,36 @@
 #pragma once
+
+#include "ve_device.hpp"
 #include "string"
 #include "vector"
 namespace ve {
+    struct PipelineConfigInfo {};
     class VePipeline
     {
     public:
-        VePipeline(const std::string &vertFilePath, const std::string &fragFilePath);
+        VePipeline(VeDevice &device,
+                   const std::string &vertFilePath,
+                   const std::string &fragFilePath,
+                   const PipelineConfigInfo &configInfo);
+        ~VePipeline(){};
+        VePipeline (const VePipeline&) = delete;
+        void operator=(const VePipeline&) = delete;
 
+        //static default pipeline config
+        static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
     private:
         static std::vector<char> readFile (const std::string& filePath);
 
-        void createGraphicsPipeline(const std::string &vertFilePath, const std::string &fragFilePath);
+        void createGraphicsPipeline(
+                const std::string &vertFilePath,
+                const std::string &fragFilePath,
+                const PipelineConfigInfo &configInfo);
+
+        void createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule);
+
+        VeDevice &veDevice;
+        VkPipeline graphicsPipeline;
+        VkShaderModule vertShaderModule;
+        VkShaderModule fragShaderModule;
     };
 }
