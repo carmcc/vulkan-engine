28841548b8fe678e711349a866a7a6538d414ba3
carmcc <carmine.marchesani@studenti.unicam.it>
2023-11-19 18:08:09 +0100

    pipeline implementation

--- first_app.hpp
+++ first_app.hpp
@@ -3,6 +3,10 @@
 #include "ve_window.hpp"
 #include "ve_pipeline.hpp"
 #include "ve_device.hpp"
+#include "ve_swap_chain.hpp"
+
+#include "memory"
+#include "vector"
 
 namespace ve {
 
@@ -11,14 +15,24 @@ namespace ve {
         static constexpr int WIDTH = 800;
         static constexpr int HEIGHT = 600;
 
-        void run();
         //describe this private member
+        FirstApp();
+        ~FirstApp();
+        FirstApp(const FirstApp&) = delete;
+        FirstApp& operator=(const FirstApp&) = delete;
+        void run();
+
     private:
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void drawFrame();
+
         VeWindow veWindow{WIDTH, HEIGHT, "Vulkan Window"};
         VeDevice veDevice{veWindow};
-        VePipeline vePipeline {veDevice,
-                               "shaders/easy_shader.vert",
-                               "shaders/easy_shader.frag",
-                               VePipeline::defaultPipelineConfigInfo(WIDTH,HEIGHT)};
+        VeSwapChain veSwapChain{veDevice, veWindow.getExtent()};
+        std::unique_ptr<VePipeline> vePipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<VkCommandBuffer> commandBuffers;
     };
 } // namespace ve
