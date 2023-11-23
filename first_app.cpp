9ee7e7f0f235505fd4b75b27206608c89869b2a9
carmcc <carmine.marchesani@studenti.unicam.it>
2023-11-23 09:08:00 +0100

    command buffer allocation

--- first_app.cpp
+++ first_app.cpp
@@ -1,5 +1,6 @@
 #include "first_app.hpp"
 #include "stdexcept"
+#include "array"
 namespace ve {
 
     FirstApp::FirstApp() {
@@ -43,6 +44,45 @@ namespace ve {
                 );
     }
 
-    void FirstApp::createCommandBuffers(){};
+    void FirstApp::createCommandBuffers(){
+        commandBuffers.resize(veSwapChain.imageCount());
+
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = veDevice.getCommandPool();
+        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+        if(vkAllocateCommandBuffers(veDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffers!");
+        }
+
+        for(int i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if(vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = veSwapChain.getRenderPass();
+            renderPassInfo.framebuffer = veSwapChain.getFrameBuffer(i);
+
+            renderPassInfo.renderArea.offset = {0, 0};
+            renderPassInfo.renderArea.extent = veSwapChain.getSwapChainExtent();
+
+
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
+            clearValues[1].depthStencil = {1.0f, 0};
+            VkClearValue clearColor = {0.0f, 0.0f, 0.0f, 1.0f};
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+        }
+    };
     void FirstApp::drawFrame() {};
 } // namespace ve
