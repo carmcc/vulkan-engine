7dea5eb06ba5a85e77311b13a154b640d7f73a16
carmcc <carmine.marchesani@studenti.unicam.it>
2023-11-25 15:19:51 +0100

    create vertex struct

--- ve_model.hpp
+++ ve_model.hpp
@@ -1,9 +1,21 @@
 #pragma once
 #include "ve_device.hpp"
+#include "glm/glm.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 namespace ve_model{
     class VeModel {
     public:
-        VeModel(VeDevice& device);
+        struct Vertex {
+            glm::vec2 position;
+            glm::vec2 color;
+
+            static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+            static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+        };
+        VeModel(VeDevice& device, const std::vector<Vertex>& vertices);
         ~VeModel();
         VeModel(const VeModel&) = delete;
         VeModel& operator=(const VeModel&) = delete;
@@ -11,6 +23,7 @@ namespace ve_model{
         void bind(VkCommandBuffer commandBuffer);
         void draw(VkCommandBuffer commandBuffer);
     private:
+        void createVertexBuffers(const std::vector<Vertex>& vertices);
         VeDevice& veDevice;
         VkBuffer vertexBuffer;
         VkDeviceMemory vertexBufferMemory;
